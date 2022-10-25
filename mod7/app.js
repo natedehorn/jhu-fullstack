a2dcd2403be1bf1d82a24dc8529b5a73b785600b
Nate DeHorn <natedehorn@gmail.com>
2022-10-24 21:23:09 -0400

    Implement additional assignment requirements

--- mod7/app.js
+++ mod7/app.js
@@ -26,20 +26,26 @@
     var service = this;
 
     var toBuy = [
-      { name: "cookies", quantity: 10 },
-      { name: "apples", quantity: 5 },
-      { name: "shrimp", quantity: 50 },
-      { name: "pineapple", quantity: 2 },
-      { name: "bread", quantity: 30 }
+      { name: "cookies", quantity: 10, pricePerItem: 2 },
+      { name: "apples", quantity: 5, pricePerItem: 1 },
+      { name: "shrimp", quantity: 50, pricePerItem: 1 },
+      { name: "pineapple", quantity: 2, pricePerItem: 5 },
+      { name: "bread", quantity: 30, pricePerItem: 2 }
     ];
 
     var alreadyBought = [];
 
     service.buyItem = function (itemIndex) {
       var item = toBuy[itemIndex];
+      this.getTotal(item);
       toBuy.splice(itemIndex, 1);
       alreadyBought.push(item);
     };
+
+    service.getTotal = function (item){
+      const formatter = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' });
+      item.total = formatter.format(item.quantity * item.pricePerItem)
+    }
 
     service.getToBuy = function () {
       return toBuy;
